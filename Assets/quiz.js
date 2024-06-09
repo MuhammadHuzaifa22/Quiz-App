a4b05ef1a5e4e8578ed1505a4f16b653548c0815
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-06-09 17:07:40 +0500

    27% work completed

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -1,4 +1,3 @@
-const prevButton = document.querySelector('#prev-button');
 const nextButton = document.querySelector('#next-button');
 const questionContainer = document.querySelector('#questions-container');
 
@@ -9,11 +8,11 @@ let currentIndex = 0;
 
 fetch('https://the-trivia-api.com/v2/questions')
 .then(response => response.json())
+console.log()
 .then(data => {
     questions = data.map(item => item.question.text);
     displayQuestion()
-    updateButtons()
-    })
+})
     .catch(error => {
         questionContainer.innerHTML = 'Failed to load questions';
         console.error('Error fetching questions:', error);
@@ -24,12 +23,17 @@ function displayQuestion(){
     }
 }
 
-function updateButtons(){
-    prevButton.disabled = currentIndex === 0;
-    nextButton.disabled = currentIndex === questions.length -1;
-}
-prevButton.addEventListener('click',()=>{
-    if(currentIndex > 0){
-        currentIndex = currentIndex -1;
+
+
+
+nextButton.addEventListener('click',()=>{
+    if(currentIndex < questions.length + 0){
+        currentIndex = currentIndex + 1;
+        displayQuestion();
+        if(currentIndex >= questions.length + 0){
+            document.write(`Questions completed.`)
+            console.log(`Questions completed.`)
+        }
     }
 })
+ 
