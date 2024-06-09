f5aac6d37d60f651414679c551cffbef9eeb7ada
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-06-09 19:21:22 +0500

    32% work completed

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -1,5 +1,6 @@
 const nextButton = document.querySelector('#next-button');
 const questionContainer = document.querySelector('#questions-container');
+const answersList = document.getElementById('answers');
 
 
 let questions = [];
@@ -20,7 +21,7 @@ fetch('https://the-trivia-api.com/v2/questions')
 function displayQuestion(){
     if(questions.length > 0){
           questionContainer.innerHTML = `
-          <h4>Question ${currentIndex + 1} of  ${questions.length}:</h4> <p>${currentIndex + 1}.) ${questions[currentIndex]}</p>
+          <h4 style="font-weight:lighter">Question <b>${currentIndex + 1}</b> of <b>${questions.length}:</b> </h4> <p>${currentIndex + 1}.) ${questions[currentIndex]}</p>
           <li>${incorrectAnswers[currentIndex]}</li>`
 
     }
@@ -33,7 +34,7 @@ nextButton.addEventListener('click',()=>{
     if(currentIndex < questions.length + 0){
         currentIndex = currentIndex + 1;
         displayQuestion();
-        if(currentIndex >= questions.length + 0){
+        if(currentIndex >= questions.length + 0 && incorrectAnswers[] > questions.length + 0){
             document.write(`Questions completed.`)
             console.log(`Questions completed.`)
         }
