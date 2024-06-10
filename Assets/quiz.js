f91e08339d19e2b45cfd3fe6d2c90ba060130052
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-06-11 00:58:09 +0500

    43% work completed

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -1,7 +1,6 @@
 const nextButton = document.querySelector('#next-button');
-// const questionContainer = document.querySelector('#question-container');
 const answersList = document.getElementById('answers');
-
+const quest = document.getElementById('quest');
 
 let questions = [];
         let currentQuestionIndex = 0;
@@ -29,7 +28,8 @@ let questions = [];
             }
             
              else {
-                document.getElementById('question-container').innerHTML = 'Questions completed';
+                quest.innerHTML = '';
+                document.getElementById('question-container').innerHTML = '<h2>Questions completed</h2>';
                 document.getElementById('answers-container').innerHTML = '';
                 document.getElementById("next-button").style.display = 'none'
                 document.getElementById("result-button").style.display = 'block'
@@ -37,18 +37,23 @@ let questions = [];
 
             }
         });
-
+const ul = document.querySelector('ul');
         function showQuestion() {
             const question = questions[currentQuestionIndex];
-            document.getElementById('question-container').innerHTML = `<h3 style="font-weight:lighter"><b>Q${currentQuestionIndex + 1}:</b> ${question.question}</h3>`;
+            document.getElementById('quest').innerHTML = `<h3 style="font-weight:lighter">Question <b>${currentQuestionIndex + 1}</b> of <b>${questions.length}:</b></h3>`
+            document.getElementById('question-container').innerHTML = `<h3 style="font-weight:lighter"><b>Q${currentQuestionIndex + 1}:</b> <b>${question.question}</b></h3>`;
             const answersContainer = document.getElementById('answers-container');
             answersContainer.innerHTML = '';
 
             const allAnswers = [...question.incorrectAnswers, question.correctAnswer];
             allAnswers.sort(() => Math.random() - 0.5); // Shuffle the answers
             allAnswers.forEach(answer => {
-                const answerElement = document.createElement('div');
-                answerElement.innerHTML = answer;
+                const answerElement = document.createElement('ul');
+                answerElement.innerHTML = `<li style="list-style:none;font-weight:lighter"><b><input style="margin-right:1%" type="radio" name="choice" class="choice" id=${answer} value=${answer}><label  for=${answer}>${answer}</label></b></li>`;
                 answersContainer.appendChild(answerElement);
             });
         }
+
+function result(){
+    window.location = 'result.html'
+}
