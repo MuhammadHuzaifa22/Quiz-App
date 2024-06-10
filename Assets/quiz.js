9a7fd001f30f646193cf73fde16c64a3e198c128
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-06-10 15:03:14 +0500

    35% work completed

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -10,35 +10,50 @@ fetch('https://the-trivia-api.com/v2/questions')
 .then(response => response.json())
 .then(data => {
     console.log(data)
-    questions = data.map(item => item.question.text);
-    incorrectAnswers = data.map( item => item.incorrectAnswers);
-    displayQuestion()
+    questions = data.map(item =>({
+        question: item.question.text,
+        incorrectAnswers: item.incorrectAnswers
+    }))
+    displayQuestion(data)
     
 })
-    .catch(error => {
-        questionContainer.innerHTML = 'Failed to load questions';
-        console.error('Error fetching questions:', error);
-    })
-function displayQuestion(){
+.catch(error => {
+    questionContainer.innerHTML = 'Failed to load questions';
+    console.error('Error fetching questions:', error);
+});
+
+
+function displayQuestion(data){
+    const questionIndex = 0; 
+    const selectedQuestion = questions[questionIndex];
+    console.log(selectedQuestion)
     if(questions.length > 0){
           questionContainer.innerHTML = `
-          <h4 style="font-weight:lighter">Question <b>${currentIndex + 1}</b> of <b>${questions.length}:</b> </h4> <p>${currentIndex + 1}.) ${questions[currentIndex]}</p>
-          <li>${incorrectAnswers[currentIndex]}</li>`
+          <h4 style="font-weight:lighter">Question <b>${currentIndex + 1}</b> of <b>${questions.length}:</b> </h4> <p>${currentIndex + 1}. ${selectedQuestion.question} </h4>`
+          displayAnswers(questions[currentIndex].incorrectAnswers);
     }
+}
+
+//   // Display incorrect answers
+function displayAnswers(answers) {
+    answersList.innerHTML = '';
+    answers.forEach(answer => {
+        const li = document.createElement('li');
+        li.textContent = answer;
+        answersList.appendChild(li);
+    });
 }
 
 
 
-
-
 nextButton.addEventListener('click',()=>{
-    if(currentIndex < questions.length + 0){
+    if(currentIndex < questions.length - 1){
         currentIndex = currentIndex + 1;
         displayQuestion();
-        if(currentIndex >= questions.length + 0  ){
-            document.write(`Questions completed.`)
-            console.log(`Questions completed.`)
-        }
+        
+    }else{
+        document.write(`Questions completed`)
     }
 })
  
+
